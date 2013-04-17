713de8a6bfb6f0ab4fb634257e14bfb011bb312a
Kasper Mackenhauer Jacobsen <k@three65.org>
2013-04-17 12:55:34 +0200

    Removed unnecessary files.

--- getFBOnly.py
+++
@@ -1,6 +0,0 @@
-import getData
-import dataWork
-
-print dataWork.insertFriends(getData.getFriends())
-print 'Getting your friends page data, please hold. This may take a while.'
-print dataWork.getPageLikes(getData.getFriendsPages())
